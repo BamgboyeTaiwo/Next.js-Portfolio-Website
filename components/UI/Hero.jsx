2d99c2e7b07c44e29b126e0bdd126deddb8c12dc
Bamgboye Taiwo <102883093+BamgboyeTaiwo@users.noreply.github.com>
2023-04-06 00:19:35 +0100

    commit: __

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from "reactstrap";
 import Image from "next/image";
 import Link from "next/link";
 import heroImg from "../../public/images/hero.jpg";
+import heroimg from '../../public/images/mypics2.jpeg'
 import classes from "../../styles/hero.module.css";
 
 const Hero = () => {
@@ -15,12 +16,15 @@ const Hero = () => {
           <Col lg="6" md="6">
             <div className={`${classes.hero__content}`}>
               <SectionSubtitle subtitle="Hello" />
-              <h2 className="mt-3 mb-3">I&apos;m Muhibur Rahman</h2>
+              <h2 className="mt-3 mb-3">I&apos;m Taiwo Bamgboye</h2>
               <h5 className="mb-4">Frontend Developer</h5>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Impedit rem sint ipsa cumque. Atque rem vel iusto impedit omnis
-                quos!
+                Efficient and results-oriented professional with 3 years of experience in demanding environments focused on
+                producing cutting-edge systems for the IT and financial remittance industry. Proven knowledge of advanced
+                technology, application development and network distribution software with a background in iOS and Android mobile
+                application development using the React Native framework. Contributing as a key team player to multi-team
+                development projects in full software development lifecycle using agile methodologies in achieving 100%
+                development objectives.
               </p>
               <div className="mt-5">
                 <button className="primary__btn">
@@ -37,7 +41,7 @@ const Hero = () => {
           {/* ========== hero img ============= */}
           <Col lg="6" md="6">
             <div className={`${classes.hero__img} text-end`}>
-              <Image alt="hero-image" src={heroImg} width="400" height="400" />
+              <Image alt="hero-image" src={heroimg} width="400" height="400" />
 
               <div className={`${classes.hero__skills}`}>
                 <h6>Skills</h6>
@@ -61,15 +65,13 @@ const Hero = () => {
                 </span>
               </div>
 
-              <div
-                className={`${classes.hero__experience} d-flex align-items-center gap-3`}
-              >
+              <div className={`${classes.hero__experience} d-flex align-items-center gap-3`}>
                 <span>
                   <i className="ri-lightbulb-flash-line"></i>
                 </span>
                 <div className="bg-transparent">
                   <h6>Experience</h6>
-                  <h5 className="mb-0">4 Years</h5>
+                  <h5 className="mb-0">3 Years</h5>
                 </div>
               </div>
             </div>
